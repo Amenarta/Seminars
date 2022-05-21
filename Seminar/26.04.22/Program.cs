3e583626d5943bf15df70fb7f08c5783ebd6bd14
Amenarta <karina1485@rambler.ru>
2022-05-21 11:44:39 +0300

    26.04.22

---
+++ Seminar/26.04.22/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+// Задача №1: Написать метод, который реализует замену элементов массива полож.на отриц.и наоборот.
+
+int [] RandomArray (int size, int min, int max) // мы хотим заполнить метод случайными значениями
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+int [] ChangeNum (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] *= (-1);
+    return array;
+}
+
+void ScreenArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int[] myArray = RandomArray(7, -9, 9);
+ScreenArray(myArray);
+ChangeNum(myArray);
+*/
+
+// Задача №2: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+int[] mas = {55, 4, 36, 27};
+void NewArray (int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == num)
+            Console.Write("Данное число " + num + " есть в массиве");
+    
+        else
+            Console.Write("Данного числа нет в массиве");
+            
+    }
+}
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+NewArray(mas, a); // задача решена не до конца
+
